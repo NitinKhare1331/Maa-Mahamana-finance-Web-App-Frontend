d3e9030209ca4023ebf38b5ac9fad8f8eb9416d1
Nitin Khare <kharenitin777@gmail.com>
2025-06-30 14:29:33 +0530

    axios instance updated for auth token

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,8 +1,14 @@
 import axios from "axios";
 
+const user = JSON.parse(localStorage.getItem("user"));
+const token = user?.token;
+
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        Authorization: token ? `Bearer ${token}` : undefined
+    }
 });
 
 export default axiosInstance;
